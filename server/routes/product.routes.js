1145f97a4569ff7c4b82137d916770f092832ccd
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-08 15:02:40 -0800

    add routes

---
+++ server/routes/product.routes.js
@@ -0,0 +1,13 @@
+// 6. Create Routes to execute functions to the DB
+
+const ProductController = require("../controllers/product.controllers");
+
+const {findAllProducts, findOneProduct, updateProduct, createNewProduct, deleteProduct} = require("../controllers/joke.controllers");
+
+module.exports = app => {
+    app.get("/api/products", findAllProducts);
+    app.get("/api/products/:id", findOneProduct);
+    app.put("/api/products/:id", updateProduct);
+    app.post("/api/products/new", createNewProduct);
+    app.delete("/app/products/delete/:id", deleteProduct);
+}
--- server/server.js
+++ server/server.js
@@ -9,4 +9,8 @@ app.use(express.json(), express.urlencoded({extended: true}))
 // 3. Connect to mongodb
 require("./config/mongoose.config");
 
+// 7. Import functions to server
+const routesFunction = require("./routes/product.routes");
+routesFunction(app);
+
 app.listen(PORT, () => console.log(`Server up on port: ${PORT}`))
