b2b09407d9e80c76de03ea6fde95efc3548638ff
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-08 13:58:01 -0800

    connect to mongodb via mongoose

--- server/server.js
+++ server/server.js
@@ -6,4 +6,7 @@ const PORT = 8000;
 // MIDDLEWARE
 app.use(express.json(), express.urlencoded({extended: true}))
 
+// 3. Connect to mongodb
+require("./config/mongoose.config");
+
 app.listen(PORT, () => console.log(`Server up on port: ${PORT}`))
