464a894d858d5aa9c96d64aa3e5ff7053e02741f
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-08 20:17:32 -0800

    add form, function to add new product to db

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ function App() {
 
   // data coming from request
   const [data, setData] = useState([]);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
 
   useEffect( () => {
     // make axios.get request to our server route
@@ -20,34 +23,47 @@ function App() {
 
   const formSubmit = (e) => {
     e.preventDefault();
-    console.log("Form Submitted");
+    console.log("Form Submitted: " + title, price, description);
+
+    // instead of the following can do as shown on line 35
+    // const newProduct = {
+    //   title : title,
+    //   price : price,
+    //   description : description
+    // }
+
+    axios.post("http://localhost:8000/api/products/new", {title, price, description})
+      .then ( res => console.log(res.data))
+      .catch ( err => console.log.log(err))
   }
 
   return (
     <div className="App">
       <h1>Data from Server [all products]</h1>
-      {JSON.stringify(data)}
+      {/* {JSON.stringify(data)} */}
+      Title: {JSON.stringify(title)} | Price: {JSON.stringify(price)} | Description {JSON.stringify(description)}
       <hr/>
 
       <form onSubmit={formSubmit}>
         <p>
-          Title: <input type="text"  />
-        </p>
-        <p>
-          Price: <input type="number" min="0.99" />
-        </p>
-        <p>
-          Descrition: <input type="text" />
+          Title: <input type="text" onChange={ (e) => setTitle(e.target.value)} value={title}/>
         </p>
 
+        <p>
+          Price: <input type="number" min="0.99" onChange={ (e) => setPrice(e.target.value)} value={price}/>
+        </p>
 
+        <p>
+          Descrition: <input type="text" onChange={ (e) => setDescription(e.target.value)} value={description}/>
+        </p>
 
         <button>Submit</button>
       </form>
+
       <hr/>
       {
         data.map( (product, i) => {
-          return <div>
+          return <div key={product._id}>
             <p>Title: {product.title} |
               Price: {product.price} |
               Description: {product.description}
